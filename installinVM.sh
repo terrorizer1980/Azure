4b26a0b0ed76349dd3a5accb65162498585f8921
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-08-20 21:59:46 -0300

    Modifications and new Docker URL

--- installinVM.sh
+++ installinVM.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
-#
+set -x
+# Install Maneger Depedencies
 apt update
 export DEBIAN_FRONTEND="noninteractive"
 apt install -y curl wget jq git
+
+# Clone and Copy Maneger Files
 git clone https://github.com/The-Bds-Maneger/Azure.git /opt/bds_core_vm 
 cp -rfv /opt/bds_core_vm/root_copy/* /  
 chmod a+x $(command -v prepare.sh)
 chmod 600 /etc/cron.d/bdscore
+
+# Install Docker EE
 curl https://get.docker.com | bash -
+
+# Install Nodejs
 curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
 apt install -y nodejs
+
+# Validate the variables
+# docker_version
+# worldname
+# worlddescripition
+# totalplayers
+# gamemode
+# difficulty
+# bdsplatfrom
+
 [ -z "$worldname" ] && worldname="bds Maneger"
 [ -z "$worlddescripition" ] && worlddescripition="NoDescription"
 [ -z "$totalplayers" ] && totalplayers="13"
 [ -z "$gamemode" ] && gamemode="survival"
 [ -z "$difficulty" ] && difficulty="normal"
 [ -z "$bdsplatfrom" ] && bdsplatfrom="bedrock"
+[ -z "$docker_version" ] && docker_version="latest"
 
 # Create JSON Config file
 (
@@ -29,6 +47,10 @@ apt install -y nodejs
     echo '"dockertag": "'${docker_version}'"'
     echo '}'
 ) | jq -r > /etc/VMAzureConfig.json
+
+# Restart systemctl
 systemctl enable bdscore
+
+# Reboot VM
 sleep 15s
 reboot
